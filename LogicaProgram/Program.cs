50be6dcc6e4980140e47f3e0955760df471b9ba6
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-16 21:00:30 -0300

    Add project files.

---
+++ LogicaProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string cidade = "São Paulo";
+
+Console.WriteLine(cidade);
+
+int idade = 20;
+Console.WriteLine(idade);
+long idadeLong = 30;
+Console.WriteLine(idadeLong);
+double temperatura = 5.60;
+Console.WriteLine(temperatura);
+float temperaturaFloat = 35.15f;
+Console.WriteLine(temperaturaFloat);
+decimal temperaturaDecimal = 35.15m;
+Console.WriteLine(temperaturaDecimal);
+char letra = 'p';
+Console.WriteLine(letra);
+
+var letraComVar = "teste";
+Console.WriteLine(letraComVar);
